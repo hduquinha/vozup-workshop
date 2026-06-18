93c0d220bcd287516a3ee5bf6688dc65f8c33fbb
hduquinha <henriqueduquis1@gmail.com>
2026-06-18 17:01:08 +0000

    fix: apontar formulário para dashboard.escolavozup.com/api/vozup/lead

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FORM_CONFIG = {
-  SERVER_URL: "/api/inscricao",
+  SERVER_URL: "https://dashboard.escolavozup.com/api/vozup/lead",
   GAS_WEB_APP_URL: "",
   SHEET_ID: "10qee1NabVgUakaZreJ9yTQ56kAz5RWk65qVo110k0Ps"
 };
